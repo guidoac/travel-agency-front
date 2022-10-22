6210ce8f4d033b23a7d047d923a91bd3464ed367
Guilherme Almada <guido.ptr13@gmail.com>
2022-10-21 23:23:50 -0300

    feat(ToursList): created ToursList prototype component

---
+++ src/components/Home/ToursList.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container mx-auto bg-white">
+        <div class="flex flex-row">
+            <p class="text-4xl text-secondary">TOURS</p>
+            <p>{{ countrySelected.description }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { defineProps, ref } from 'vue';
+
+const props = defineProps({
+    tours: {
+        type: Object,
+        required: true
+    },
+
+    countrySelected: {
+        type: Object,
+        required: true
+    }
+});
+
+const tours = ref(props.tours);
+console.log('props', tours.value.tours);
+</script>
+
+<style lang="scss" scoped></style>
