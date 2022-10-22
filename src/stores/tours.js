f9608c4a1038cdf30b778ec18dd901a20c33a334
Guilherme Almada <guido.ptr13@gmail.com>
2022-10-22 14:25:52 -0300

    feat(tours): added company alias to tours request

--- src/stores/tours.js
+++ src/stores/tours.js
@@ -1,5 +1,6 @@
 import { defineStore } from 'pinia';
 import plugin from '@/plugins/http';
+import { useCompaniesStore } from './companies';
 
 export const useToursStore = defineStore('tours', {
     state: () => ({
@@ -7,12 +8,17 @@ export const useToursStore = defineStore('tours', {
     }),
     actions: {
         async fetchTours(countryId) {
+            const companies = useCompaniesStore();
+
             try {
-                const tours = await plugin.$api.get('/company1/tours', {
-                    params: {
-                        country: countryId
+                const tours = await plugin.$api.get(
+                    `${companies.company.alias}/tours`,
+                    {
+                        params: {
+                            country: countryId
+                        }
                     }
-                });
+                );
 
                 console.log(tours);
                 this.tours = tours.data;
