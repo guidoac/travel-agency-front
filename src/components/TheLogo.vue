f9052a898cad2bca6035ce991a9f9de84d840c66
Guilherme Almada <guido.ptr13@gmail.com>
2022-11-09 21:14:14 -0300

    feat(Pages): created initial HomePage and RegisterPage

---
+++ src/components/TheLogo.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="bg-slate-300 p-6 logo">Minha Logo</div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+.logo {
+    width: 150px;
+}
+</style>
--- src/router/admin.js
+++ src/router/admin.js
@@ -1,13 +1,26 @@
 import LoginPage from '@/pages/Admin/LoginPage';
+import RegisterPage from '@/pages/Admin/RegisterPage';
+import HomePage from '@/pages/Admin/HomePage';
 
 export default [
     {
-        name: 'admin',
+        path: '/admin',
+        redirect: '/admin/home',
         children: [
             {
                 name: 'admin.login',
-                path: '/login',
+                path: 'login',
                 component: LoginPage
+            },
+            {
+                name: 'admin.register',
+                path: 'register',
+                component: RegisterPage
+            },
+            {
+                name: 'admin.home',
+                path: 'home',
+                component: HomePage
             }
         ]
     }
